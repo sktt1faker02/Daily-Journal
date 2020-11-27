cc718ef8fd0f546b9ddd5ea036d78a2f4fbdb0d3
Michael Tabilin <denver.guillermo22@gmail.com>
2020-11-27 22:41:14 +0800

    Second Commit

--- app.js
+++ app.js
@@ -71,6 +71,6 @@ posts.forEach(function(post){
 
 
 
-app.listen(processe.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
